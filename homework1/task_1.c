6987c0a461d8f447085b741e017fb07bfa94208b
WoWaster <wowasterdev@gmail.com>
2021-09-16 20:59:14 +0300

    Combine functions' declaration and definition

--- homework1/task_1.c
+++ homework1/task_1.c
@@ -10,29 +10,6 @@
  * computeQuotient find a quotient using only addition, subtraction and multiplication.
  * Shouldn't be used directly
  */
-int computeQuotient(int dividend, int divisor);
-
-/**
- * Helper function for computeQuotient which checks input and decides how to properly get value
- * based on dividend's and divisor's signs
- */
-int solveForQuotient(int dividend, int divisor);
-
-int main()
-{
-    int a = 0;
-    int b = 0;
-
-    printf("Данная программа вычисляет неполное частное от деления a на b.\n");
-    inputSingleInt("Введите число a: ", &a);
-    inputSingleInt("Введите число b: ", &b);
-
-    int result = solveForQuotient(a, b);
-
-    printf("Неполное частное от деления %d на %d равно %d.\n", a, b, result);
-    return 0;
-}
-
 int computeQuotient(int dividend, int divisor)
 {
     int quotient = 0;
@@ -43,6 +20,10 @@ int computeQuotient(int dividend, int divisor)
     return quotient;
 }
 
+/**
+ * Helper function for computeQuotient which checks input and decides how to properly get value
+ * based on dividend's and divisor's signs
+ */
 int solveForQuotient(int dividend, int divisor)
 {
     if (divisor == 0) {
@@ -58,4 +39,19 @@ int solveForQuotient(int dividend, int divisor)
         return -computeQuotient(abs(dividend), divisor) - 1;
     else
         return computeQuotient(abs(dividend), abs(divisor)) + 1;
+}
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    printf("Данная программа вычисляет неполное частное от деления a на b.\n");
+    inputSingleInt("Введите число a: ", &a);
+    inputSingleInt("Введите число b: ", &b);
+
+    int result = solveForQuotient(a, b);
+
+    printf("Неполное частное от деления %d на %d равно %d.\n", a, b, result);
+    return 0;
 }
