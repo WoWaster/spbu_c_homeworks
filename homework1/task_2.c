c2a8b70816104fa0f4ce9cd61a11cc3b399578c0
WoWaster <wowasterdev@gmail.com>
2021-09-24 13:37:34 +0300

    Refactor findMaxNonUniqueElement

--- homework1/task_2.c
+++ homework1/task_2.c
@@ -2,25 +2,24 @@
  * Найдите максимальный элемент массива, встречающийся более одного раза (массив неупорядоченный).
  */
 #include "../library/io.h"
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * WARNING: No check for array of unique values, INT_MIN as return value may be faulty
- */
 int findMaxNonUniqueElement(const int* arr, int length)
 {
-    int max = INT_MIN;
+    int max = arr[0];
+    int position = -1;
     for (int i = 0; i < length; ++i) {
         int count = 1;
         for (int j = i + 1; j < length; ++j) {
             if (arr[i] == arr[j])
                 count++;
         }
-        if (count > 1 && max < arr[i])
+        if (count > 1 && max <= arr[i]) {
             max = arr[i];
+            position = i;
+        }
     }
-    return max;
+    return position;
 }
 
 int main()
