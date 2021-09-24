c8368d2e3c97eb93661028d446b3ef0ee1653a1f
WoWaster <wowasterdev@gmail.com>
2021-09-24 17:46:25 +0300

    Add LinkedMap struct to LinkedMap lib

--- library/LinkedMap.c
+++ library/LinkedMap.c
@@ -1,5 +1,10 @@
 #include "LinkedMap.h"
 
+struct LinkedMap {
+    Node* first;
+    Node* last;
+};
+
 struct Node {
     const char* key;
     int value;
--- library/LinkedMap.h
+++ library/LinkedMap.h
@@ -1,4 +1,7 @@
 #ifndef SPBU_C_HOMEWORKS_LINKEDMAP_H
 #define SPBU_C_HOMEWORKS_LINKEDMAP_H
+
+typedef struct LinkedMap LinkedMap;
 typedef struct Node Node;
+
 #endif // SPBU_C_HOMEWORKS_LINKEDMAP_H
