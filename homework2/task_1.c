3c30aa7b969dcf26582f952c50da38240967943f
WoWaster <wowasterdev@gmail.com>
2021-09-24 20:41:11 +0300

    Add task_1 from homework2

---
+++ homework2/task_1.c
@@ -0,0 +1,45 @@
+#include "../library/LinkedMap.h"
+#include <stdio.h>
+
+void fillMapFromFile(FILE* file, LinkedMap* map)
+{
+    char buffer[127] = "";
+    while (fscanf(file, "%s", buffer) != EOF) {
+        if (hasKey(map, buffer)) {
+            put(map, buffer, getValue(map, buffer) + 1);
+        } else
+            put(map, buffer, 1);
+    }
+}
+
+void generateCSVFromMap(LinkedMap* map, const char* filename)
+{
+    FILE* output = fopen(filename, "w");
+    Node* currentElement = map->first;
+    while (currentElement != NULL) {
+        fprintf(output, "%s,%d\n", currentElement->key, currentElement->value);
+        currentElement = currentElement->nextElement;
+    }
+    fclose(output);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3) {
+        printf("Usage: HW2_Task_1 input_file output_file\n");
+        return 0;
+    } else if (argc > 3) {
+        printf("Too many arguments!\n");
+        return 0;
+    }
+
+    FILE* input = fopen(argv[1], "r");
+    LinkedMap* words = newMap();
+
+    fillMapFromFile(input, words);
+    generateCSVFromMap(words, argv[2]);
+
+    fclose(input);
+    freeMap(words);
+    return 0;
+}
