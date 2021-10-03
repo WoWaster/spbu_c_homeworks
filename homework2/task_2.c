522621b5b8e2d484579bf8ce0f313a4bd2c29ad8
WoWaster <wowasterdev@gmail.com>
2021-10-03 20:56:28 +0300

    Add task 2 from homework 2

---
+++ homework2/task_2.c
@@ -0,0 +1,65 @@
+#include "../library/String.h"
+#include "../library/io.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void processData(char* inputFile, char* outputFile)
+{
+    FILE* input = fopen(inputFile, "r");
+    FILE* output = fopen(outputFile, "w");
+
+    int M = 0;
+    fscanf(input, "%d", &M);
+
+    char buffer[128] = "";
+    fscanf(input, "%s", buffer);
+    String* string = newString(buffer);
+
+    int N = 0;
+    fscanf(input, "%d", &N);
+
+    for (int i = 0; i < 30; ++i) {
+        char command[8] = "";
+        char arg1[64] = "";
+        char arg2[64] = "";
+
+        fscanf(input, "%s", command);
+        fscanf(input, "%s", arg1);
+        fscanf(input, "%s", arg2);
+
+        if (strcmp(command, "REPLACE") == 0) {
+            replace(string, arg1, arg2);
+        } else if (strcmp(command, "INSERT") == 0) {
+            insert(string, arg1, arg2);
+        } else if (strcmp(command, "DELETE") == 0) {
+            delete (string, arg1, arg2);
+        }
+
+        char* charString = stringToChar(string);
+        fprintf(output, "%s\n", charString);
+        free(charString);
+    }
+
+    fclose(input);
+    fclose(output);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3) {
+        printf("Usage: %s input_file output_file\n", argv[0]);
+        return 0;
+    } else if (argc > 3) {
+        printf("Too many arguments!\n");
+        return 0;
+    }
+    if (!isFileExists(argv[1])) {
+        printf("Input file does not exists.");
+        return 0;
+    }
+
+    processData(argv[1], argv[2]);
+
+    return 0;
+}
